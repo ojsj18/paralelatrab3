f5a4eb00378c662dc284870a4f4220a8b7535068
ojsj18 <juhnior88@gmail.com>
2022-12-17 13:40:48 -0300

    doing

--- prog2.c
+++ prog2.c
@@ -1,35 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#define TAG 1
 
 void main(int argc, char* argv[]) {
+
   int size, rank;
-  long number;    
+  long elements;
+  long nMsg;
+  long tMsg;
+  long ni;    
+
+  long msgS;
+  long msgR;
+
   MPI_Status stat;
 
-  long ping[];
-  long pong[];
-  //preenche o ping se for o 1
-  //preenche o pong se for o 2 
   MPI_Init(&argc, &argv) ;
   MPI_Comm_size (MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  if(size > 2){
+    printf("só são permitidos 2 processos \n");
+    exit(0);
+  }
+
+  if (argc != 3)
+	{
+		printf("usage: %s <nmsg> <tmsg>\n",argv[0]);
+		exit(0);
+	}
+	else
+	{
+		nMsg = atoi(argv[1]);
+  	printf("<nmsg>: %ld \n",nMsg);
+
+		if (nMsg%2 != 0)
+		{
+			printf("<nmsg>: %ld (precisa ser um numero par!!)\n",nMsg);
+			exit(0);
+		}
+    tMsg = atoi(argv[2]);
+    if (tMsg%sizeof(long) != 0)
+		{
+			printf("<tmsg>: %ld (precisa ser um numero multiplo de 8!!)\n",tMsg);
+			exit(0);
+		}
+    ni = tMsg/8;
+
+	}
+
+  long *ping = (long *) malloc( nMsg*sizeof(long) );
+  long *pong = (long *) malloc( nMsg*sizeof(long) );
 
   if(size > 2){
     exit(0);
   }
-
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   
   if (rank == 0) {
-    printf("Processor 0 sends a message to 1\n");
-    number = 123456789 ;
-    MPI_Send(&number, 1, MPI_LONG, 1, TAG, MPI_COMM_WORLD);
-  
+      elements = 1;
+      for(int i=0;i<nMsg/2;i++){
+        ping[i] = elements;
+        elements+= 1;
+      }
+    printf(" %ld %ld \n",nMsg, ni);
+    for(int i=0;i<nMsg/2;i+=ni){
+      MPI_Send(&ping[i], ni, MPI_LONG, 1, 0, MPI_COMM_WORLD);
+      printf("ping: enviado a %d msg\n",i);
+      MPI_Recv(&pong[i], ni, MPI_LONG, 1, 0, MPI_COMM_WORLD, &stat);
+      printf("ping: recebido a %d msg\n",i);
+    }
+      printf("vetor pong recebido: ");
+      for(int i = 0;i<nMsg/2;i++){
+        printf("%ld ",pong[i]);
+      }
+      printf("\n");
+
   } else if (rank == 1) {
-    printf("Processor 1 receives a message from 0\n");
-    MPI_Recv(&number, 1, MPI_LONG, 0, TAG, MPI_COMM_WORLD, &stat);
-    printf("I received the value: %ld \n", number);
+      elements = (nMsg/2) + 1;
+      for(int i=0;i<nMsg/2;i++){
+        pong[i] = elements;
+        elements+= 1;
+      }
+      printf(" %ld %ld \n",nMsg, ni);
+      for(int i=0;i<nMsg/2;i+=ni){
+        MPI_Recv(&ping[i], ni, MPI_LONG, 0, 0, MPI_COMM_WORLD, &stat);
+        printf("pong: recebido a %d msg\n",i);
+        MPI_Send(&pong[i], ni, MPI_LONG, 0, 0, MPI_COMM_WORLD);
+        printf("pong: enviado a %d msg\n",i);
+      }
+      printf("vetor ping recebido: ");
+      for(int i = 0;i<nMsg/2;i++){
+        printf("%ld ",ping[i]);
+      }
+      printf("\n");
   }
   
   MPI_Finalize();    
